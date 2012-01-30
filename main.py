f8127a8d6b568244e725606e93c2d708603990d4
Cris Anderson <anderson861@gmail.com>
2012-01-30 16:47:57 -0500

    pallets and uprights are done

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ deep = 1 #single deep
 bay = 3 #pallets per bay
 bays = 10 #length in bays
 aisles = 5 #number of aisles
-lapproach = 845 #lower approach of the exyz crane
-uapproach = 2045 #upper approach min
+levels = 5 #loads in y
+lapproach = 830 #lower approach of the exyz crane
+uapproach = 410 #upper approach min
 aislew = 1500 #width of aisle
 backgap = 300 #back to back pallet gap
 
@@ -19,13 +20,17 @@ backgap = 300 #back to back pallet gap
 palx = 1016 #pallet width
 paly = 1220 #pallet length
 palz = 144 #pallet height
-loadx = 1016 #load width
-loady = 1220 #load length
-loadz = 1016 #load height (excluding pallet)
+loadx = palx #1016 #load width --- logic to pick the bigger of the two not implemented yet
+loady = paly #1220 #load length
+loadz = 1500 #load height (excluding pallet)
 palgap = 70 #gap between pallets
 upgap = 80 #gap between load and upright
 upx = 100 #upright width in the x direction
 upy = 100 #in the y (z when considering HBW as a whole)
+up2up = 800 #width between uprights (looking down aisle)
+datz = 60 #down aisle tie height
+daty = 20 #down aisle tie width
+lift = 140 #lift off gap
 
 def createblock(bname, x,y,z):
     '''Block adding boilerplate-pass it the name of the block as a string and where to stick it.'''
@@ -60,23 +65,55 @@ dwg.blocks.add(pallet)
 blockfaces(pallet, palx, paly, palz, 0, 0, 0)
 
 # Outermost loop for creating levels
+for m in range(levels):
+    levelheight = datz + palz + loadz + lift
+    lh = levelheight * m
+    # Outer loop to create all the aisles
+    for l in range(aisles):
+        # Calc distance between aisles
+        aislewidth = (2 * paly) + aislew + backgap
+        aisle2 = aislewidth * l
+        # Inner loop to create one aisle
+        for k in range(bays):
+            # calculate bay length
+            blen = upx + (2 * upgap) + (3 * loadx) + (2 * palgap)
+            blength = blen * k
+            # create first bay
+            for i in range(bay):
+                blockref = dxf.insert(blockname='pallet', insert=((i*(loadx+palgap)+upx+upgap)+blength,(aislew/2)+aisle2,lapproach+lh))
+                dwg.add(blockref)
+            # create opposite side
+            for j in range(bay):
+                blockref = dxf.insert(blockname='pallet', insert=((j*(loadx+palgap)+upx+upgap)+blength,0-(aislew/2)-loady+aisle2,lapproach+lh))
+                dwg.add(blockref)
+
+# Uprights
+upright = dxf.block(name='upright')
+dwg.blocks.add(upright)
+# calculate upright height
+upz = lapproach + (levels * (palz + loadz + lift + datz)) - datz + uapproach
+blockfaces(upright, upx, upy, upz, 0, 0, 0)
 # Outer loop to create all the aisles
 for l in range(aisles):
     # Calc distance between aisles
     aislewidth = (2 * paly) + aislew + backgap
     aisle2 = aislewidth * l
     # Inner loop to create one aisle
-    for k in range(bays):
+    for k in range(bays+1):
         # calculate bay length
         blen = upx + (2 * upgap) + (3 * loadx) + (2 * palgap)
         blength = blen * k
-        # create first bay
-        for i in range(bay):
-            blockref = dxf.insert(blockname='pallet', insert=((i*(loadx+palgap)+upx+upgap)+blength,(aislew/2)+aisle2,lapproach))
-            dwg.add(blockref)
-        # create opposite side
-        for j in range(bay):
-            blockref = dxf.insert(blockname='pallet', insert=((j*(loadx+palgap)+upx+upgap)+blength,0-(aislew/2)-loady+aisle2,lapproach))
-            dwg.add(blockref)
+        # create inner upright
+        blockref1 = dxf.insert(blockname='upright', insert=((blength,(aislew/2)+(loady/2)+(up2up/2)+aisle2, 0)))
+        dwg.add(blockref1)
+        # create outer upright
+        blockref2 = dxf.insert(blockname='upright', insert=((blength,(aislew/2)+(loady/2)-(up2up/2)-upy+aisle2, 0)))
+        dwg.add(blockref2)
+        # create inner upright (opposite)
+        blockref1 = dxf.insert(blockname='upright', insert=((blength,0-(aislew/2)-(loady/2)+(up2up/2)+aisle2, 0)))
+        dwg.add(blockref1)
+        # create outer upright (opposite)
+        blockref2 = dxf.insert(blockname='upright', insert=((blength,0-(aislew/2)-(loady/2)-(up2up/2)-upy+aisle2, 0)))
+        dwg.add(blockref2)
 
 dwg.save()
